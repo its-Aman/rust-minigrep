f65659e8a877ababf6a1f86fd82bcc06f479a586
_AmAn_K_ <kumaraman7056@hotmail.com>
2020-09-14 23:06:14 +0530

    updated.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::{env, process};
 
-use minigrep;
+use minigrep_by_aman::{run, Config};
 
 fn main() {
-    let config = minigrep::Config::new(env::args()).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application Error, {}", e);
         process::exit(1);
     };
